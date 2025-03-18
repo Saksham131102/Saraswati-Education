32479358bb252e285fe61267cfa50d89121b75a3
Saksham <113179290+Saksham131102@users.noreply.github.com>
2025-03-19 00:33:12 +0530

    update

--- server/server.js
+++ server/server.js
@@ -29,10 +29,10 @@ app.use('/api/testimonials', require('./routes/testimonials'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 }
 
